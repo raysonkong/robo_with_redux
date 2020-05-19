66ec2813639db8d591966281f78b07830afddbca
raysonkong <kong.rayson@gmail.com>
2020-05-19 10:41:08 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   render() {
     return(
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1>RoboFriends Rock!!!!</h1>
         <Card />
         <Card />
         <Card />
